4f2426ae5b70e702129f36bcd6d4df64d3e32215
Ryan Cobb <ryan.cobb@mx.com>
2017-03-12 17:14:29 -0600

    begin javascript

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,6 +2,28 @@
 //= require materialize
 //= require_tree .
 
-$(function() {
-	$(".button-collapse").sideNav();
-});
+window.onload = function() {
+
+	var items = document.querySelectorAll(".timeline li");
+
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+			);
+	}
+
+	function callbackFunc() {
+		debugger;
+		for (var i = 0; i < items.length; i++) {
+			if (isElementInViewport(items[i])) {
+				items[i].classList.add("in-view");
+			}
+		}
+	}
+
+	window.addEventListener("scroll", callbackFunc());
+}
