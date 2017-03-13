ee05ef5376dc9f93c3c993a406c8a061953f0c6c
Ryan Cobb <ryan.david.cobb@gmail.com>
2017-03-12 21:16:06 -0600

    working timeline

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,29 +1,33 @@
 //= require jquery.min
 //= require materialize
 //= require_tree .
+var items = []
 
 window.onload = function() {
+	items = document.querySelectorAll(".timeline li");
+	callbackFunc();
+}
 
-	var items = document.querySelectorAll(".timeline li");
+function isElementInViewport(el) {
+	var rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+}
 
-	function isElementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-		return (
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-			);
-	}
-
-	function callbackFunc() {
-		debugger;
-		for (var i = 0; i < items.length; i++) {
-			if (isElementInViewport(items[i])) {
-				items[i].classList.add("in-view");
-			}
+function callbackFunc() {
+	for (var i = 0; i < items.length; i++) {
+		if (isElementInViewport(items[i])) {
+			items[i].classList.add("in-view");
+		}
+		else {
+			items[i].classList.remove("in-view");
 		}
 	}
-
-	window.addEventListener("scroll", callbackFunc());
 }
+
+window.addEventListener("scroll", callbackFunc);
+window.addEventListener("resize", callbackFunc);
