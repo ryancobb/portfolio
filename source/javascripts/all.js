a600de09ea1ae5ea7c64f76f4a0857760354ff33
Ryan Cobb <ryan.david.cobb@gmail.com>
2018-12-24 12:42:15 -0700

    ?

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,6 @@
 //= require jquery.min
-//= require materialize
 //= require_tree .
+
 var items = []
 
 window.onload = function() {
@@ -11,9 +11,9 @@ window.onload = function() {
 function isElementInViewport(el) {
 	var rect = el.getBoundingClientRect();
 	return (
-		rect.top >= 0 &&
+		(rect.top + 150) >= 0 &&
 		rect.left >= 0 &&
-		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		(rect.bottom - 150) <= (window.innerHeight || document.documentElement.clientHeight) &&
 		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 		);
 }
@@ -22,9 +22,6 @@ function callbackFunc() {
 	for (var i = 0; i < items.length; i++) {
 		if (isElementInViewport(items[i])) {
 			items[i].classList.add("in-view");
-		}
-		else {
-			items[i].classList.remove("in-view");
 		}
 	}
 }
